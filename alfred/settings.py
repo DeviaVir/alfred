79c2cacde5895c70b9afbf7154e2d65fb1d78713
Chase Sillevis <chase@sillevis.net>
2017-08-16 11:29:32 +0200

    customize the secret

--- alfred/settings.py
+++ alfred/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rk5qtfn9cf@15emci*y)&c^331%322c9ate#($zq(o^30)=w34'
+SECRET_KEY = os.getenv('SECRET_KEY', 'rk5qtfn9cf@15emci*y)&c^331%322c9ate#($zq(o^30)=w34')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', True)
